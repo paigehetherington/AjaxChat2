1cf21d63de1078e06ed43184652c46b78ecc1be3
Paige Hetherington <herbmama@gmail.com>
2016-10-17 10:14:19 -0400

    initial import

---
+++ src/com/theironyard/message.java
@@ -0,0 +1,30 @@
+package com.theironyard;
+
+/**
+ * Created by vajrayogini on 10/17/16.
+ */
+public class message {
+    int id;
+    String author;
+    String text;
+    Timestamp time;
+
+    public message(int id, String author, String text, Timestamp time) {
+        this.id = id;
+        this.author = author;
+        this.text = text;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
